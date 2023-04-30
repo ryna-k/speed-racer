4d645916e6c698370bb0e29bb2797372953d942f
ryna-k <102335559+ryna-k@users.noreply.github.com>
2023-04-30 12:33:56 -0400

    Add files via upload

--- js/Form.js
+++ js/Form.js
@@ -7,7 +7,7 @@ class Form {
   }
 
   setElementsPosition() {
-    this.titleImg.position(120, 140);
+    this.titleImg.position(120, 50);
     this.input.position(width / 2 - 110, height / 2 - 80);
     this.playButton.position(width / 2 - 90, height / 2 - 20);
     this.greeting.position(width / 2 - 300, height / 2 - 100);
@@ -26,18 +26,21 @@ class Form {
     this.input.hide();
   }
 
-  handleMousePressed() { 
-    this.playButton.mousePressed(() => { this.input.hide(); 
-      this.playButton.hide(); 
-      var message = ` Hello ${this.input.value()} </br>wait for another player to join...`; 
-      this.greeting.html(message); 
-      playerCount += 1; 
-      player.name = this.input.value(); 
-      player.index = playerCount; 
-      player.addPlayer(); 
-      player.updateCount(playerCount); 
-      player.getDistance(); 
-    }); }
+  handleMousePressed() {
+    this.playButton.mousePressed(() => {
+      this.input.hide();
+      this.playButton.hide();
+      var message = `
+      Hello ${this.input.value()}
+      </br>wait for another player to join...`;
+      this.greeting.html(message);
+      playerCount += 1;
+      player.name = this.input.value();
+      player.index = playerCount;
+      player.addPlayer();
+      player.updateCount(playerCount);
+    });
+  }
 
   display() {
     this.setElementsPosition();
